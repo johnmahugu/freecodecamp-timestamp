effc267beef83d6c63bd6cdac963408b8efe9f91
johnmahugu <johnmahugu@gmail.com>
2024-07-22 18:49:26 +0300

    Freecode Camp First commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get("/api/:date?", (req, res) => {
+  let dateString = req.params.date;
+  let date;
+
+  if (!dateString) {
+    date = new Date();
+  } else {
+    if (!isNaN(dateString)) {
+      dateString = parseInt(dateString);
+    }
+    date = new Date(dateString);
+  }
+
+  if (date.toString() === 'Invalid Date') {
+    res.json({ error: 'Invalid Date' });
+  } else {
+    res.json({
+      unix: date.getTime(),
+      utc: date.toUTCString()
+    });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
